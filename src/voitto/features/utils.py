0cc1501841f57786a8b95eff67ec16e339b8f0d5
Ahti Helminen <ahtihelminen@gmail.com>
2026-01-06 16:18:19 +0200

    feat: helper for per-player features

--- src/voitto/features/utils.py
+++ src/voitto/features/utils.py
@@ -7,3 +7,16 @@ def shifted_rolling_mean(
     window: int
 ) -> pd.Series:
     return x.shift(shift).rolling(window).mean()
+
+def get_mean_feature_last_n_games(
+    unified: pd.DataFrame,
+    feature_col: str,
+    n_games: int
+) -> pd.Series:
+    """
+    Calculates per-player the shifted rolling mean for a given feature over
+    the last n games.
+    """
+    return unified.groupby("player_name")[feature_col].transform(
+        shifted_rolling_mean, shift=1, window=n_games
+    )
